65e97251863f84b253372b5b03fe49f7e4adb945
Michelle Lim <limxlmichelle@gmail.com>
2022-01-26 17:30:15 -0500

    Style the individual workflow cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,29 +45,38 @@ export default function Home({
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center">
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          {allTasksData
-            .filter(
-              ({ title, description, tags }) =>
-                title.toLowerCase().includes(query.toLowerCase()) ||
-                description.toLowerCase().includes(query.toLowerCase()) ||
-                tags.find((tag) =>
-                  tag.toLowerCase().includes(query.toLowerCase())
-                )
-            )
-            .map(({ id, title, description, tags }) => (
-              <a
-                href={`/tasks/${id}`}
-                key={id}
-                className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-              >
-                <h3 className="text-2xl font-bold">{title} &rarr;</h3>
-                <p className="mt-4 text-xl">{description}</p>
-                <p className="mt-4 text-sm">{tags.join(", ")}</p>
+      <div className="flex flex-wrap justify-around py-5">
+        {allTasksData
+          .filter(
+            ({ title, description, tags }) =>
+              title.toLowerCase().includes(query.toLowerCase()) ||
+              description.toLowerCase().includes(query.toLowerCase()) ||
+              tags.find((tag) =>
+                tag.toLowerCase().includes(query.toLowerCase())
+              )
+          )
+          .map(({ id, title, description, tags }) => (
+            <Link href={`/tasks/${id}`} key={id}>
+              <a className="p-6 m-6 border border-white/30 w-96 rounded-md bg-white bg-opacity-10 hover:bg-opacity-30">
+                <h3 className="min-h-15 text-xl text-white font-bold h-16 line-clamp-2">
+                  {title}
+                </h3>
+                <p className="mt-4 text-gray-300 text-l line-clamp-4 h-20">
+                  {description}
+                </p>
+                <div className="flex">
+                  {tags.map((tag, id) => (
+                    <div
+                      key={id}
+                      className="rounded-full text-white bg-white bg-opacity-20 px-5 mr-2 text-sm flex flex-col justify-center text-center"
+                    >
+                      {tag}
+                    </div>
+                  ))}
+                </div>
               </a>
-            ))}
-        </div>
+            </Link>
+          ))}
       </div>
     </Layout>
   );
