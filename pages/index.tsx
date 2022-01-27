f639e20152ce97a93a5458eb2eda0b5e422ec818
Michelle Lim <limxlmichelle@gmail.com>
2022-01-27 16:04:00 -0500

    Extract WorkflowCard into its own component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,41 @@ import { GetStaticProps } from "next";
 import { getSortedWorkflowsData } from "../lib/workflows";
 import Layout, { siteTitle } from "../components/layout";
 
+interface Workflow {
+  id: string;
+  title: string;
+  description: string;
+  tags: string[];
+}
+
+const WorkflowCard = ({ id, title, description, tags }: Workflow) => (
+  <Link href={`/workflows/${id}`} key={id}>
+    <a className="p-6 m-6 border border-white/30 w-96 rounded-md bg-white bg-opacity-10 hover:bg-opacity-30 active:bg-opacity-50">
+      <h3 className="min-h-15 text-xl text-white font-bold h-16 line-clamp-2">
+        {title}
+      </h3>
+      <p className="mt-4 text-gray-300 text-l line-clamp-4 h-20">
+        {description}
+      </p>
+      <div className="flex">
+        {tags.map((tag, id) => (
+          <div
+            key={id}
+            className="rounded-full text-white bg-white bg-opacity-20 px-5 mr-2 text-sm flex flex-col justify-center text-center"
+          >
+            {tag}
+          </div>
+        ))}
+      </div>
+    </a>
+  </Link>
+);
+
 export default function Home({
   allWorkflowsData,
 }: {
-  allWorkflowsData: {
-    id: string;
-    title: string;
-    description: string;
-    tags: string[];
-  }[];
+  allWorkflowsData: Workflow[];
 }) {
-  // Note that we are only extracting select fields from the spec data since
-  // users are most likely going to search for metadata rather than the command contents.
-
-  const [query, setQuery] = useState("");
-
   return (
     <Layout home>
       <Head>
@@ -36,47 +56,10 @@ export default function Home({
             Commands.dev is a beautiful, searchable index of popular terminal
             commands for developers.
           </div>
-          <div className="mt-4 h-12 relative">
-            <input
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              type="text"
-            />
-          </div>
         </div>
       </div>
       <div className="flex flex-wrap justify-around py-5">
-        {allWorkflowsData
-          .filter(
-            ({ title, description, tags }) =>
-              title.toLowerCase().includes(query.toLowerCase()) ||
-              description.toLowerCase().includes(query.toLowerCase()) ||
-              tags.find((tag) =>
-                tag.toLowerCase().includes(query.toLowerCase())
-              )
-          )
-          .map(({ id, title, description, tags }) => (
-            <Link href={`/workflows/${id}`} key={id}>
-              <a className="p-6 m-6 border border-white/30 w-96 rounded-md bg-white bg-opacity-10 hover:bg-opacity-30">
-                <h3 className="h-15 text-xl text-white font-bold line-clamp-2">
-                  {title}
-                </h3>
-                <p className="mt-1 text-gray-300 text-l line-clamp-4 h-24">
-                  {description}
-                </p>
-                <div className="flex mt-1 flex-wrap">
-                  {tags.map((tag, id) => (
-                    <div
-                      key={id}
-                      className="rounded-full text-white bg-white bg-opacity-20 px-5 mr-2 text-sm flex flex-col justify-center text-center"
-                    >
-                      {tag}
-                    </div>
-                  ))}
-                </div>
-              </a>
-            </Link>
-          ))}
+        {allWorkflowsData.map((workflow) => WorkflowCard(workflow))}
       </div>
     </Layout>
   );
