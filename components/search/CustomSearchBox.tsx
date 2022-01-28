9e05fc7fe025160c51fe8eb4fe7ad5b913dd65d1
Michelle Lim <limxlmichelle@gmail.com>
2022-01-28 18:35:33 -0500

    Change input box back to white

--- components/search/CustomSearchBox.tsx
+++ components/search/CustomSearchBox.tsx
@@ -8,7 +8,7 @@ function SearchBox({ refine }: { refine: Dispatch<SetStateAction<string>> }) {
       type="search"
       placeholder="Click or press 'Ctrl + K' to search"
       onChange={(e) => refine(e.currentTarget.value)}
-      className="w-screen md:w-2/3 bg-sky-700 h-9 cursor-pointer border border-white/30 px-3 rounded-lg text-sm text-white placeholder:text-gray-300 focus:outline-none"
+      className="w-screen md:w-2/3 h-9 cursor-pointer border border-white/30 px-3 rounded-lg text-sm focus:outline-none"
     />
   );
 }
