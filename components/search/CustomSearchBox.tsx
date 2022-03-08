7f71cb205b1b7024295fd16e9375663b7850ac14
Ian Hodge <ian@warp.dev>
2022-03-08 16:34:10 -0500

    MVP

--- components/search/CustomSearchBox.tsx
+++ components/search/CustomSearchBox.tsx
@@ -1,8 +1,19 @@
 import { connectSearchBox } from "react-instantsearch-dom";
 import { Dispatch, SetStateAction } from "react";
 import { SearchIcon } from "../icons/search";
+import { useHotkeys } from "react-hotkeys-hook";
 
 function SearchBox({ refine }: { refine: Dispatch<SetStateAction<string>> }) {
+  useHotkeys("ctrl+k", () => {
+    if (document != null) {
+      let searchBar = document.querySelector(
+        "#algolia_search"
+      ) as HTMLInputElement;
+      if (searchBar != null) {
+        searchBar.focus();
+      }
+    }
+  });
   return (
     <div className="dark:bg-card-dark bg-card-light w-screen h-9 cursor-pointer rounded-sm flex items-center">
       <div className="h-9 flex items-center text-icon-light dark:text-icon-dark pl-3 pr-1 dark:bg-card-dark bg-search-bar-light">
