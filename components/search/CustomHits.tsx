e16fa23bc27c69e001047d26853da54a252c9d5f
Ian Hodge <ian@warp.dev>
2022-03-08 19:04:34 -0500

    addresing comments

--- components/search/CustomHits.tsx
+++ components/search/CustomHits.tsx
@@ -31,7 +31,7 @@ function Hits({
         <div className="flex flex-col items-center">
           <div className="w-72 h-32 bg-[url('../img/no-search-light.png')] dark:bg-[url('../img/no-search-dark.png')]"></div>
           <div className="text-2xl text-null-search-text dark:text-white pt-4">
-            No results matching
+            No matching results
           </div>
         </div>
       </div>
