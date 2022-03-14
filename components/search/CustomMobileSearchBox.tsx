b7bbf871986e501b83c14372ae1ef49d18cb72bd
Ian Hodge <ian@warp.dev>
2022-03-14 13:34:03 -0400

    still playing around with mobile stuff

--- components/search/CustomMobileSearchBox.tsx
+++ components/search/CustomMobileSearchBox.tsx
@@ -19,13 +19,13 @@ function SearchBox({
   }, []);
 
   return (
-    <div className="dark:bg-card-dark bg-search-bar-light grow h-9 cursor-pointer rounded-sm items-center flex dark:text-white">
+    <div className="dark:bg-card-dark bg-search-bar-light grow h-9 cursor-pointer rounded-sm items-center flex text-black dark:text-white">
       <input
         id={"algolia_search_mobile"}
         type="search"
         placeholder={"Search commands"}
         onChange={(e) => refine(e.currentTarget.value)}
-        className="grow h-9 cursor-pointer rounded-sm text-sm focus:outline-none dark:bg-card-dark bg-search-bar-light placeholder:opacity-50 dark:text-white px-2"
+        className="grow h-9 cursor-pointer text-sm focus:outline-none dark:bg-card-dark bg-search-bar-light placeholder:opacity-50 text-black dark:text-white px-2"
       />
       <button
         className="h-9 flex items-center text-icon-light dark:text-icon-dark pr-3 pl-1 dark:bg-card-dark bg-search-bar-light"
