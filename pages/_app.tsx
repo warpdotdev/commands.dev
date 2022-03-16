18ca4160d81c9e210662ce2e6fb831bf8aaabf85
Michelle Lim <limxlmichelle@gmail.com>
2022-03-16 15:29:45 -0400

    Record navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { useRouter } from "next/router";
 import * as gtag from "../lib/gtag";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       {/* According to documentation - this should be defined inline to avoid FOUC (flash of unstyled content) 
