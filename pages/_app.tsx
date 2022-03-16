04099d560a18e4cd349b83767f5974d3512ef509
Michelle Lim <limxlmichelle@gmail.com>
2022-03-16 15:29:09 -0400

    Set up Google Analytics on app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import * as gtag from "../lib/gtag";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -18,7 +19,26 @@ function MyApp({ Component, pageProps }: AppProps) {
               document.documentElement.classList.remove('dark');
             }`}
       </Script>
-      <Component {...pageProps} />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />;
     </>
   );
 }
