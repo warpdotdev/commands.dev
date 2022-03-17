d69a5a1c19e2199aeb8fa11872900bd40025f50b
Ian Hodge <ian@warp.dev>
2022-03-17 17:20:56 -0400

    adding the image

--- components/layout.tsx
+++ components/layout.tsx
@@ -29,12 +29,7 @@ export default function Layout({
           content="Commands.dev: a beautiful, searchable index of popular terminal commands for developers"
         />
         {/* TODO: Ask Shikhiu for Workflows image preview */}
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content={`../img/commands-preview.svg`} />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
