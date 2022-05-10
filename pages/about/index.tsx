e687cc74d0c65e09e950be83bcd4a44056c8d83e
Ian Hodge <ian@warp.dev>
2022-05-10 15:59:15 -0400

    adding title

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -7,6 +7,9 @@ export default function About() {
   return (
     <Layout>
       <main className="grow">
+        <Head>
+          <title>About</title>
+        </Head>
         <div className="bg-cover text-black dark:text-white">
           <div className="py-10 px-3 max-w-2xl">
             <h1 className="md:text-3xl text-3xl font-bold pb-3">About</h1>
