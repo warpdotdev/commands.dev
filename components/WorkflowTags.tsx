78b71facb3a52f0c11f49d65b7aaa7917fe941a4
Jeff Lloyd <jeff@warp.dev>
2024-11-01 13:09:50 -0700

    Upgrade react, react-dom, and next

--- components/WorkflowTags.tsx
+++ components/WorkflowTags.tsx
@@ -4,10 +4,11 @@ export default function WorkflowTags({ tags }: { tags: string[] }) {
   return (
     <div className="flex mt-1 flex-wrap">
       {tags.map((tag, id) => (
-        <Link key={id} href={`/categories/${tag}`}>
-          <a className="rounded-full text-black dark:text-white bg-pill-light dark:bg-pill-dark px-5 my-1 mr-2 text-sm flex flex-col justify-center text-center hover:opacity-60">
-            {tag}
-          </a>
+        <Link
+          key={id}
+          href={`/categories/${tag}`}
+          className="rounded-full text-black dark:text-white bg-pill-light dark:bg-pill-dark px-5 my-1 mr-2 text-sm flex flex-col justify-center text-center hover:opacity-60">
+          {tag}
         </Link>
       ))}
     </div>
